3380afbe35523900e810e15b3969c756b741ec2d
Brandon Bunch <brandon_bunch@icloud.com>
2020-03-11 07:57:09 -0500

    add base html for app

---
+++ src/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <div className="App-header">
+            <h1>Easy Initiative</h1>
+            <h1>logo</h1>
+            <p>More fun, less hastle</p>
+
+        </div>
+    )
+}
+
+export default Header
