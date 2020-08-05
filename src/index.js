d5fec96772c4c00567019515975dd07c3449e7c5
Brandon Bunch <brandonbunch@Brandons-MBP.attlocal.net>
2020-08-05 17:20:30 -0500

    canning aws-exports temporarily

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify'
-import config from './aws-exports'
-Amplify.configure(config)
+// import config from './aws-exports'
+// Amplify.configure(config)
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 
